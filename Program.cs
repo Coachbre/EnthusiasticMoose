4b7d3a05a00433c15c501a95594794b63dff283a
Coachbre <brekonan1@gmail.com>
2021-05-21 17:07:42 -0400

    added mooseSays function

--- Program.cs
+++ Program.cs
@@ -1,5 +1,43 @@
 ﻿using System;
 
-        {
-Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!--------------------------------------------");
-        }
+
+Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+
+void MooseSays()
+{
+    // @ is added in front of the string to allow a multi line string
+    Console.WriteLine(@"
+
+                                      _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+                       
+                    ");
+}
+
+MooseSays();
