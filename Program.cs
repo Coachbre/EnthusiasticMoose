20bb75aaf3400913f1b996b7d2ed4e01e0123905
Coachbre <brekonan1@gmail.com>
2021-05-23 21:59:56 -0400

    completed phase 5

--- Program.cs
+++ Program.cs
@@ -41,6 +41,39 @@ void MooseSays(string message)
                     ");
 }
 
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    // prints interpolated question + (Y/N)
+    string answer = Console.ReadLine().ToLower();
+    // read users answer, and converts to lowercase string
+    while (answer != "y" && answer != "n")
+    // while/when the users answer doesnt equal "y" or "n"
+
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+        // print question once more and read users answer again
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return  false;
+    }
+    //if answer is equal to "y" then return true. If not, return false
+}
+
+
 MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
 // {message} is replaced with the string that is passed in when function is called
+
+bool isTrue = MooseAsks("Is Canada real?");
+// {question} is replaced with string passed into the function
+Console.WriteLine(isTrue);
+// console prints question/boolean
